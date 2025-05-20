fd2826cae3dfab1b215ba82e83c8d929221ea07a
Ahsaan-ullah3236 <158459990+Ahsaan-ullah3236@users.noreply.github.com>
2025-05-20 17:17:30 +0500

    change the About us page/ Globel presence

--- src/app/globalPresance/page.tsx
+++ src/app/globalPresance/page.tsx
@@ -37,8 +37,8 @@ const GlobalPressance=()=>{
             </div>
     
            
-            <div className="lg:pl-8 -pt-6 lg:pt-0">
-                <Image src={LahoreOffice} alt="lahore-office" height={200} width={260} className="mx-auto" />
+            <div className="lg:pl-8 pt-6 lg:pt-0">
+                <Image src={LahoreOffice} alt="lahore-office" height={220} width={260} className="mx-auto sm:mx-0" />
               <h2 className="font-semibold text-black-700 mt-2"  style={{fontSize:"15px"}}>Lahore Office</h2>
               <address className="text-black-600  font-semibold not-italic"  style={{fontSize:"15px"}}>
                 MindSees PVT LTD
@@ -46,14 +46,17 @@ const GlobalPressance=()=>{
                 <a href="https://www.google.com/maps/place/MindSees/@31.4642669,74.2988289,17z/data=!3m1!4b1!4m6!3m5!1s0x8562bef8b6b348af:0x2228a06e52354095!8m2!3d31.4642624!4d74.3014038!16s%2Fg%2F11mcwzy2ny?entry=ttu&g_ep=EgoyMDI1MDQxNi4xIKXMDSoASAFQAw%3D%3D" target="_blank"
             rel="noopener noreferrer">
               <div  className="text-black-600 mt-1 flex" style={{fontSize:"11px"}}>
+    
               <span className="mr-1"><MapPin className="h-6 mt-1 w-6 "/></span> 30-B, Malik Tufail Rd, Block B1 Block A 1 Phase 1 Johar Town, Lahore, 54770, Pakistan
               </div>
+    
+    
                 </a>
+              
             </div>
     
-
-            <div className="lg:pl-8 -pt-6 lg:pt-0">
-                <Image src={USAOffice} alt="USA-office" height={200} width={260} className="mx-auto"/>
+            <div className="lg:pl-8 pt-6 lg:pt-0">
+                <Image src={USAOffice} alt="USA-office"   height={220} width={260} className="mx-auto sm:mx-0" />
               <h2 className="font-semibold text-black-700 mt-2" style={{fontSize:"15px"}}>USA Office</h2>
               <address className="text-black-600 font-semibold not-italic" style={{fontSize:"15px"}}>
                 MindSees LLC
